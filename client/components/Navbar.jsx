5dc1305663568e9cd707db35d3a22894ded61d8e
Ramon Reyes <reyesjunk@gmail.com>
2017-12-03 14:03:34 -0500

    changed Nav component to Navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../public/stylesheet.css';
 
-export default function Nav(){
+export default function Navbar(){
   return(
     <nav id="primary_nav_wrap">
     <ul>
@@ -11,7 +11,6 @@ export default function Nav(){
         <li><a href="#">HTML</a></li>
         <li><a href="#">CSS</a>
             <ul>
-                
                 <li><a href="#">Layout</a>
                 </li>
                 <li><a href="#">Grid</a></li>
